0715e76a77880c8c70df59c137b0cbb4f9aedbd0
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-06-05 01:22:07 -0700

    added a basic overlay with readingtext across the screen - needs bugfixes and styling, but it works

--- src/pages/Train/backup.js
+++ src/pages/Train/backup.js
@@ -1,31 +1,30 @@
 const [isFetching, setIsFetching] = useState(false);
 
-  const handleButtonClick = () => {
-    setIsFetching(true);
-    // Perform any necessary actions here
-    setTimeout(() => {
-      setIsFetching(false);
-    }, 3000); // Simulating a delay before resetting the state
-  };
+const handleButtonClick = () => {
+  setIsFetching(true);
+  // Perform any necessary actions here
+  setTimeout(() => {
+    setIsFetching(false);
+  }, 3000); // Simulating a delay before resetting the state
+};
 
-  return (
-    <div style={cardStyle}>
-      <Button
-        className="btn-danger"
-        variant="secondary"
-        id="dropdown-battle"
-        onClick={handleButtonClick}
-        disabled={isFetching}
-      >
-        {isFetching ? (
-          <img
-            src="https://media3.giphy.com/media/dGD5YHl8xW6c/giphy.gif?cid=ecf05e479h8f5shzhb7oypucnalyj5v7bg5quyve5p2dpznb&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-            alt="fetching-pokemon"
-          />
-        ) : (
-          "Catch 'em All"
-        )}
-      </Button>
-    </div>
-  );
-}
+return (
+  <div style={cardStyle}>
+    <Button
+      className="btn-danger"
+      variant="secondary"
+      id="dropdown-battle"
+      onClick={handleButtonClick}
+      disabled={isFetching}
+    >
+      {isFetching ? (
+        <img
+          src="https://media3.giphy.com/media/dGD5YHl8xW6c/giphy.gif?cid=ecf05e479h8f5shzhb7oypucnalyj5v7bg5quyve5p2dpznb&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+          alt="fetching-pokemon"
+        />
+      ) : (
+        "Catch 'em All"
+      )}
+    </Button>
+  </div>
+);
